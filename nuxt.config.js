79c93b798e7f2293e245191c1e316e40286b3672
wahengchang <wahengchang@gmail.com>
2020-02-13 18:02:25 +0800

    update favicon.ico

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,7 @@ module.exports = {
       { src: 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/x-icon', href: '/logo.jpg' },
       { rel: 'stylesheet', href: '/assets/css/main.css' }
     ]
   },
